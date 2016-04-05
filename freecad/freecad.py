a853e7d0d304da9f64c3dd98f98ecd3e0bbe9fe3
rsecchi <r.secchi@gmail.com>
2016-04-05 01:00:50 +0100

    Arguments for script

--- freecad/freecad.py
+++ freecad/freecad.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
 
-print('Hello world')
+import cgitb, sys, cgi
+
+cgitb.enable()
+
+#print('Content-type: text/html\n')
+
+print('Content-type: application/sla')
+print('Content-Disposition: inline; filename="Tazzina.stl"\n')
+
+#data = open('Tazzina.stl', 'rb').read()
+#sys.stdout.write(data)
+
+args = cgi.FieldStorage()
+
+#for i in arguments.keys():
+#	if ( i == "filename" ):
+#		filename = arguments[i].value
+
+if 'filename' in args.keys():
+	filename = args['filename'].value
+	data = open('Tazzina.stl', 'rb').read()
+	sys.stdout.write(data)
+
+
 
